ba9a607a4086830dbc67a592d059e768936c54b2
Shawn Mitchell <shawn.mitchell>
2022-01-27 11:52:29 -0500

    0.0.13

--- src/__tests__/ZKAgeProof.test.ts
+++ src/__tests__/ZKAgeProof.test.ts
@@ -1,4 +1,4 @@
-import {doZKProof, getZKHash, getZKProof} from '../index';
+import { doZKProof, getZKHash, getZKProof } from '../index';
 
 const secretPhrase = 'ROBERT SHAWN MITCHELL';
 const zk = '0x04b54f1e5142e91281e86eef2d9cf3c7c6edca18b9b3d548a52afcc118171353';
@@ -10,9 +10,9 @@ test('Do ZK Proof', () => {
 test('Do Partial Proof', () => {
   const hash = getZKHash(21, new Date(), new Date('18/Mar/1968'), secretPhrase);
   expect(doZKProof(21, hash, zk)).toBe(true);
-})
+});
 
 test('Do False Proof', () => {
   const hash = getZKHash(65, new Date(), new Date('18/Mar/1968'), secretPhrase);
   expect(doZKProof(65, hash, zk)).toBe(false);
-})
+});
